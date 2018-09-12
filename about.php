686a2685fb31f108e73c837726fab787d9ba1851
K Mac <kmcdon83@gmail.com>
2018-09-11 20:55:27 -0400

    Update about.php

--- about.php
+++ about.php
@@ -5,6 +5,7 @@ require_once DVWA_WEB_PAGE_TO_ROOT . 'dvwa/includes/dvwaPage.inc.php';
 
 dvwaPageStartup( array( 'phpids' ) );
 
+
 $page = dvwaPageNewGrab();
 $page[ 'title' ]   = 'About' . $page[ 'title_separator' ].$page[ 'title' ];
 $page[ 'page_id' ] = 'about';
